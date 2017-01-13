ed870a1b0cc329473fa78be08df1b0d11a048271
Alexander Matos <alexander0885@gmail.com>
2017-01-13 18:50:12 +0000

    playing around

--- server.js
+++ server.js
@@ -2,9 +2,15 @@ var express=require('express');
 
 var app = express();
 
-app.get('/', function (req, res) {
+/*app.get('/', function (req, res) {
   res.send('Hello World!');
 })
+*/
+app.get('/:txt',function(req,res){
+  var txt=req.params.txt;
+  res.send("Hello World!, " + txt);
+  
+});
 
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
